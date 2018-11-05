134f7d7e1cb51f2489a5c483a69fcdcc6719268a
KKambi <bok03220@gmail.com>
2018-11-05 19:02:13 +0900

    친구기능 정리완료

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,24 @@
+class FriendsController < ApplicationController
+  before_action :set_friend, only: :destroy
+
+
+  def index
+  	@friends = current_user.friends
+
+    @incomings = FriendRequest.where(friend: current_user)
+    @outgoings = current_user.friend_requests
+  end
+
+  def destroy
+	  current_user.remove_friend(@friend)
+    # head :no_content
+    redirect_to friends_index_path
+  end
+
+
+private
+  def set_friend
+    @friend = current_user.friends.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+
   root 'cosmetics#index' 
+  
+
+
+  # 친구 요청 관리 라우팅
+  resources :friend_requests
+  
+  # 친구 관리 라우팅
+  get 'friends/index'
+  delete 'friends/destroy/:id' => 'friends#destroy', :as => 'friends_destroy'
+
 
   devise_for :users
 
-	resources :cosmetics, except: [:index]
-  
+
+
+
+	resources :cosmetics, except: [:index]  
   get '/cosmetics/users/:user_id' => 'cosmetics#index', as: 'index_cosmetic'
 end
