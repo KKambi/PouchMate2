7f370af058a3921891f95a4e5f9d7d5d2fe39193
KKambi <bok03220@gmail.com>
2018-11-15 19:42:30 +0900

    라우팅 짜잘한 오류 수정

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_11_12_085140) do
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "writer_id"
-    t.string "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   create_table "bests", force: :cascade do |t|
     t.integer "cosmetic_id"
     t.integer "user_id"
@@ -53,12 +46,19 @@ ActiveRecord::Schema.define(version: 2018_11_12_085140) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "writer_id"
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cosmetic_infos", force: :cascade do |t|
     t.string "name"
     t.string "info_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
   create_table "cosmetics", force: :cascade do |t|
