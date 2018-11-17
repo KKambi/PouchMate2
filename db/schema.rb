962eaa02073e2dd4a3f5f8a87ed8e418b61aaabe
KKambi <bok03220@gmail.com>
2018-11-17 15:39:40 +0900

    18.11.17 피드 완성 및 화장대 분리되어있던 것을 합침

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_15_111640) do
 
   create_table "bests", force: :cascade do |t|
@@ -28,7 +27,6 @@ ActiveRecord::Schema.define(version: 2018_11_15_111640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "carousels", force: :cascade do |t|
     t.string "name"
